70a0d5f21dd05dd5426bcc85de7ec95ae8919165
Yash Raj <hiiamyashraj@gmail.com>
2026-08-04 01:36:30 +0530

    Change config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
   images: {
     unoptimized: true,
   },
-  // Speed up dev builds
-  swcMinify: true,
-  reactStrictMode: false,
 }
 
 module.exports = nextConfig
